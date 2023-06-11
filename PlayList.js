d483177c6d181a1ed7cdef4f5da09a43279e12ef
AbdalrahmanEldeep <abdo.eslam2021@gmail.com>
2023-06-11 08:24:41 +0300

    UPDATE DESIGN && APPEND SMALL MEDIA DESIGN

--- PlayList.js
+++ PlayList.js
@@ -25,6 +25,82 @@ export const PlayList = () => `
                 </button>
                 </div>
             </div>
+            <div class="card">
+                <div class="card-img">
+                    <div class="img-box">
+                        <img src="https://e-cdns-images.dzcdn.net/images/cover/2e018122cb56986277102d2041a592c8/250x250-000000-80-0-0.jpg" loading="lazy"/>
+                    </div>
+                    <div class="card-text">
+                        <div class="music-name">Music Name</div>
+                        <div class="artist-name">Artist Name</div>
+                    </div>
+                </div>
+                <div class="controll">
+                <button class="play-btn">
+                <span class="material-symbols-rounded">play_circle</span>
+                </button>
+                <button class="like-btn">
+                    <span class="material-symbols-rounded">recommend</span>
+                </button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-img">
+                    <div class="img-box">
+                        <img src="https://e-cdns-images.dzcdn.net/images/cover/2e018122cb56986277102d2041a592c8/250x250-000000-80-0-0.jpg" loading="lazy"/>
+                    </div>
+                    <div class="card-text">
+                        <div class="music-name">Music Name</div>
+                        <div class="artist-name">Artist Name</div>
+                    </div>
+                </div>
+                <div class="controll">
+                <button class="play-btn">
+                <span class="material-symbols-rounded">play_circle</span>
+                </button>
+                <button class="like-btn">
+                    <span class="material-symbols-rounded">recommend</span>
+                </button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-img">
+                    <div class="img-box">
+                        <img src="https://e-cdns-images.dzcdn.net/images/cover/2e018122cb56986277102d2041a592c8/250x250-000000-80-0-0.jpg" loading="lazy"/>
+                    </div>
+                    <div class="card-text">
+                        <div class="music-name">Music Name</div>
+                        <div class="artist-name">Artist Name</div>
+                    </div>
+                </div>
+                <div class="controll">
+                <button class="play-btn">
+                <span class="material-symbols-rounded">play_circle</span>
+                </button>
+                <button class="like-btn">
+                    <span class="material-symbols-rounded">recommend</span>
+                </button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-img">
+                    <div class="img-box">
+                        <img src="https://e-cdns-images.dzcdn.net/images/cover/2e018122cb56986277102d2041a592c8/250x250-000000-80-0-0.jpg" loading="lazy"/>
+                    </div>
+                    <div class="card-text">
+                        <div class="music-name">Music Name</div>
+                        <div class="artist-name">Artist Name</div>
+                    </div>
+                </div>
+                <div class="controll">
+                <button class="play-btn">
+                <span class="material-symbols-rounded">play_circle</span>
+                </button>
+                <button class="like-btn">
+                    <span class="material-symbols-rounded">recommend</span>
+                </button>
+                </div>
+            </div>
         </div>
      </div>
   </div>
